73f4173fabaae2d7386ccc15ad504364690f7d83
dbudim <budim.dmitriy@pdffiller.team>
2021-10-15 12:13:58 +0300

    up grid 4 images

--- src/test/java/IosDemoTest.java
+++ src/test/java/IosDemoTest.java
@@ -1,17 +1,14 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import emulators.IosHub;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static io.appium.java_client.MobileBy.AccessibilityId;
+import static org.openqa.selenium.By.xpath;
 
 /**
  * Created by dbudim on 25.06.2021
@@ -28,10 +25,10 @@ public class IosDemoTest {
 
     @Test
     public void testSum() {
-        $(AccessibilityId("IntegerA")).sendKeys("2");
-        $(AccessibilityId("IntegerB")).sendKeys("3");
-        $(AccessibilityId("ComputeSumButton")).click();
-        $(AccessibilityId("Answer")).should(Condition.text("5"));
+        $(xpath("//*[@name='IntegerA']")).sendKeys("2");
+        $(xpath("//*[@name='IntegerB']")).sendKeys("3");
+        $(xpath("//*[@name='ComputeSumButton']")).click();
+        $(xpath("//*[@name='Answer']")).should(Condition.text("5"));
     }
 
     @AfterMethod
