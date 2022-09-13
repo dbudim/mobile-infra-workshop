7f3550f9290549fc3e8b961eb5397ad61a40def4
dbudim <dmitriy.budim@gmail.com>
2022-09-13 17:22:27 +0300

    change app

--- src/main/java/emulators/AndroidHub.java
+++ src/main/java/emulators/AndroidHub.java
@@ -19,19 +19,19 @@ public class AndroidHub implements WebDriverProvider {
 
     private static final String SELENOID_URL = "http://192.168.0.111:4444/wd/hub";
     private static final String GGR_URL = "http://aqa:123456@192.168.0.111:4445/wd/hub";
-    private static final String APP_URL = "https://github.com/appium/appium/raw/master/sample-code/apps/ApiDemos-debug.apk";
+    private static final String APP_URL = "https://github.com/wikipedia-qa/wikipedia-selenide/raw/master/mobile/src/test/resources/apks/org.wikipedia.apk";
 
     @Override
     public WebDriver createDriver(@Nonnull DesiredCapabilities desiredCapabilities) {
         var capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("android");
+        capabilities.setBrowserName("Android");
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("version", "10.0");
         capabilities.setCapability(NEW_COMMAND_TIMEOUT, 90);
         capabilities.setCapability("uiautomator2ServerInstallTimeout", 90000);
         capabilities.setCapability(ORIENTATION, "PORTRAIT");
         capabilities.setCapability(AUTOMATION_NAME, "UiAutomator2");
-        capabilities.setCapability("appActivity", ".ApiDemos");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", APP_URL);
         try {
             return new AndroidDriver(new URL(SELENOID_URL), capabilities);
--- src/test/java/AndroidDemoTest.java
+++ src/test/java/AndroidDemoTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import emulators.AndroidHub;
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,7 +9,6 @@ import org.testng.annotations.Test;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static io.appium.java_client.MobileBy.AccessibilityId;
 
 /**
  * Created by dbudim on 25.06.2021
@@ -26,9 +26,9 @@ public class AndroidDemoTest {
 
     @Test
     public void testDemo() {
-        $(AccessibilityId("Animation")).click();
-        $(AccessibilityId("Cloning")).click();
-        $(AccessibilityId("Run")).shouldBe(Condition.visible);
+        $(By.xpath("//*[@content-desc='Search Wikipedia'][1]")).click();
+        $(By.xpath("//*[@content-desc='Clear query']")).shouldBe(Condition.visible);
+        $(By.xpath("//*[@content-desc='Search Wikipedia'][1]")).shouldBe(Condition.visible);
     }
 
     @AfterMethod
